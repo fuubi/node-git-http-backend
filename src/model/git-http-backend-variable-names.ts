f039f72c50b388db1a56d1155743f56c80b1b3bc
Fuubi <fabriparrillo@hotmail.com>
2020-05-01 19:49:02 +0200

    add git backend variable names

---
+++ src/model/git-http-backend-variable-names.ts
@@ -0,0 +1,21 @@
+import { GitHttpBackendVariable } from './git-http-backend-variable'
+
+export const gitHttpBackendVariableNames : GitHttpBackendVariable[] = [
+    'QUERY_STRING' ,
+    'REMOTE_USER' ,
+    'CONTENT_LENGTH' ,
+    'HTTP_CONTENT_ENCODING' ,
+    'REMOTE_USER' ,
+    'REMOTE_ADDR' ,
+    'GIT_COMMITTER_NAME' ,
+    'GIT_COMMITTER_EMAIL' ,
+    'CONTENT_TYPE' ,
+    'PATH_INFO' ,
+    'GIT_PROJECT_ROOT' ,
+    'PATH_TRANSLATED' ,
+    'SERVER_PROTOCOL' ,
+    'REQUEST_METHOD' ,
+    'GIT_HTTP_EXPORT_ALL' ,
+    'GIT_HTTP_MAX_REQUEST_BUFFER'
+]
+
