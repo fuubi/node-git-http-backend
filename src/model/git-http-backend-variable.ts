50514d40fc48201840103926f67da809be7d7022
Fuubi <fabriparrillo@hotmail.com>
2020-05-01 19:48:42 +0200

    add git http backend variable

---
+++ src/model/git-http-backend-variable.ts
@@ -0,0 +1,33 @@
+export type GitHttpBackendVariable =
+    'QUERY_STRING'
+    |
+    'REMOTE_USER'
+    |
+    'CONTENT_LENGTH'
+    |
+    'HTTP_CONTENT_ENCODING'
+    |
+    'REMOTE_ADDR'
+    |
+    'GIT_COMMITTER_NAME'
+    |
+    'GIT_COMMITTER_EMAIL'
+    |
+    'CONTENT_TYPE'
+    |
+    'PATH_INFO'
+    |
+    'GIT_PROJECT_ROOT'
+    |
+    'PATH_TRANSLATED'
+    |
+    'SERVER_PROTOCOL'
+    |
+    'REQUEST_METHOD'
+    |
+    'GIT_HTTP_EXPORT_ALL'
+    |
+    'GIT_HTTP_MAX_REQUEST_BUFFER'
+
+
+
